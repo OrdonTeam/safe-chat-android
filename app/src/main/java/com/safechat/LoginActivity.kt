ca04f5e87b9a02dda3b74ecce753b9c5f68e2733
Karol Kowalski <ordon1313@gmail.com>
2016-05-14 15:34:18 +0200

    login-core

--- app/src/main/java/com/safechat/LoginActivity.kt
+++ app/src/main/java/com/safechat/LoginActivity.kt
@@ -1,11 +1,13 @@
 package com.safechat
 
 import android.os.Bundle
+import com.safechat.login.LoginController
 import com.safechat.login.LoginViewImpl
 
 class LoginActivity : BaseActivity() {
 
     val loginView = LoginViewImpl()
+    val loginController = LoginController()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
