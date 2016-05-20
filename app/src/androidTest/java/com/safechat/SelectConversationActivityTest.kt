06df77892605f9df88d1729072b066ed2324a0be
Karol Kowalski <ordon1313@gmail.com>
2016-05-20 09:18:55 +0200

    android test

--- app/src/androidTest/java/com/safechat/SelectConversationActivityTest.kt
+++ app/src/androidTest/java/com/safechat/SelectConversationActivityTest.kt
@@ -1,3 +1,16 @@
 package com.safechat
 
-class SelectConversationActivityTest
+import android.support.test.rule.ActivityTestRule
+import org.junit.Rule
+import org.junit.Test
+
+class SelectConversationActivityTest {
+
+    @JvmField @Rule
+    val rule = ActivityTestRule(SelectConversationActivity::class.java)
+
+    @Test
+    fun shouldStartSelectConversationActivity() {
+
+    }
+}
