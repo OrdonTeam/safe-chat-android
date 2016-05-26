3fbedceb9226f0acae2096616d2ee4510a32b8cc
Karol Kowalski <ordon1313@gmail.com>
2016-05-26 19:47:16 +0200

    shouldCheckIfKeyExists

--- registration-core/src/main/java/com/safechat/register/RegisterController.kt
+++ registration-core/src/main/java/com/safechat/register/RegisterController.kt
@@ -1,8 +1,11 @@
 package com.safechat.register
 
-class RegisterController(val registerView: RegisterView) {
+class RegisterController(
+        val registerView: RegisterView,
+        val registerRepository: RegisterRepository) {
 
     fun onViewCreated() {
+        registerRepository.isKeySaved()
         registerView.successLogIn()
     }
 }
--- registration-core/src/test/java/com/safechat/register/RegisterControllerTest.kt
+++ registration-core/src/test/java/com/safechat/register/RegisterControllerTest.kt
@@ -6,10 +6,17 @@ import org.mockito.Mockito.*
 class RegisterControllerTest {
 
     val registerView = mock(RegisterView::class.java)
+    val registerRepository = mock(RegisterRepository::class.java)
 
     @Test
     fun shouldCallOnLogInWhenKeyAlreadyExists() {
-        RegisterController(registerView).onViewCreated()
+        RegisterController(registerView, registerRepository).onViewCreated()
         verify(registerView, times(1)).successLogIn()
+    }
+
+    @Test
+    fun shouldCheckIfKeyExists() {
+        RegisterController(registerView, registerRepository).onViewCreated()
+        verify(registerRepository, times(1)).isKeySaved()
     }
 }
