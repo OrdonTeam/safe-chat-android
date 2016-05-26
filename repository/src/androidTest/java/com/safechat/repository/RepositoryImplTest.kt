571bf41c385dd717ebbb49d2afbbb0e2b5615588
Karol Kowalski <ordon1313@gmail.com>
2016-05-26 23:07:09 +0200

    shouldFindKeyAfterKeySaved

--- repository/src/androidTest/java/com/safechat/repository/RepositoryImplTest.kt
+++ repository/src/androidTest/java/com/safechat/repository/RepositoryImplTest.kt
@@ -1,12 +1,31 @@
 package com.safechat.repository
 
-import org.junit.Assert
+import android.support.test.InstrumentationRegistry.getTargetContext
+import org.junit.After
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Before
 import org.junit.Test
+import java.security.KeyPair
 
 class RepositoryImplTest {
 
+    val repositoryImpl = RepositoryImpl(getTargetContext())
+
     @Test
-    fun shouldReturnFalse() {
-        Assert.assertFalse(RepositoryImpl().isKeySaved())
+    fun shouldNotFindKeyBeforeKeySaved() {
+        assertFalse(repositoryImpl.isKeySaved())
+    }
+
+    @Test
+    fun shouldFindKeyAfterKeySaved() {
+        repositoryImpl.saveNewKey(KeyPair(null, null))
+        assertTrue(repositoryImpl.isKeySaved())
+    }
+
+    @After
+    @Before
+    fun clearKey() {
+        repositoryImpl.sharedPreferences.edit().remove("key_pair").apply()
     }
 }
--- repository/src/main/java/com/safechat/repository/RepositoryImpl.kt
+++ repository/src/main/java/com/safechat/repository/RepositoryImpl.kt
@@ -1,14 +1,23 @@
 package com.safechat.repository
 
+import android.content.Context
+import android.preference.PreferenceManager
 import com.safechat.register.RegisterRepository
 import java.security.KeyPair
 
-class RepositoryImpl : RegisterRepository {
+class RepositoryImpl(context: Context) : RegisterRepository {
+
+    val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
 
     override fun isKeySaved(): Boolean {
-        return false
+        return sharedPreferences.contains(KEY_PAIR)
     }
 
     override fun saveNewKey(key: KeyPair) {
+        sharedPreferences.edit().putString(KEY_PAIR, KEY_PAIR).apply()
+    }
+
+    companion object {
+        val KEY_PAIR = "key_pair"
     }
 }
