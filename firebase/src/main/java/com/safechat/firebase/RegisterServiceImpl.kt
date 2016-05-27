92d183d811cf53f28e9674b07b8e5b24f5880c6e
Karol Kowalski <ordon1313@gmail.com>
2016-05-27 23:32:11 +0200

    register public key in firebase

--- firebase/src/main/java/com/safechat/firebase/RegisterServiceImpl.kt
+++ firebase/src/main/java/com/safechat/firebase/RegisterServiceImpl.kt
@@ -4,19 +4,37 @@ import com.google.firebase.database.FirebaseDatabase
 import com.safechat.encryption.toBase64String
 import com.safechat.register.RegisterService
 import rx.Observable
+import rx.Subscriber
 import java.security.PublicKey
 
 class RegisterServiceImpl : RegisterService {
 
     override fun registerNewKey(publicKey: PublicKey): Observable<Unit> {
         return Observable.create { subscriber ->
+            OnSubscribe(subscriber, publicKey).doOnSubscribe();
+        }
+    }
+
+    private class OnSubscribe(val subscriber: Subscriber<in Unit>, val publicKey: PublicKey) {
+        fun doOnSubscribe() {
             subscriber.onStart()
-            val database = FirebaseDatabase.getInstance()
-            val myRef = database.getReference("message");
-            myRef.setValue(publicKey.toBase64String()).addOnSuccessListener {
-                subscriber.onNext(Unit)
-                subscriber.onCompleted()
-            };
+            FirebaseDatabase
+                    .getInstance()
+                    .reference
+                    .child("users")
+                    .child(publicKey.toBase64String())
+                    .setValue(true)
+                    .addOnSuccessListener(onSuccess)
+                    .addOnFailureListener(onError)
+        }
+
+        val onSuccess: (Void) -> Unit = {
+            subscriber.onNext(Unit)
+            subscriber.onCompleted()
+        }
+        val onError: (Exception) -> Unit = {
+            subscriber.onError(it)
+            subscriber.onCompleted()
         }
     }
 }
