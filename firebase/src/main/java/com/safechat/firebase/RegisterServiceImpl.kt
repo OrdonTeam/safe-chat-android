6e57fb83774a72f489ea6abf8cb74fd4817e3a8f
Karol Kowalski <ordon1313@gmail.com>
2016-06-01 17:30:00 +0200

    register -> login -> put user

--- firebase/src/main/java/com/safechat/firebase/RegisterServiceImpl.kt
+++ firebase/src/main/java/com/safechat/firebase/RegisterServiceImpl.kt
@@ -1,41 +1,68 @@
 package com.safechat.firebase
 
+import android.util.Log
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 import com.safechat.encryption.toBase64String
 import com.safechat.register.RegisterService
 import rx.Observable
 import rx.Subscriber
 import java.security.PublicKey
-import java.util.*
 
 class RegisterServiceImpl : RegisterService {
 
     override fun registerNewKey(publicKey: PublicKey): Observable<Unit> {
         return Observable.create { subscriber ->
-            OnSubscribe(subscriber, publicKey).doOnSubscribe();
+            OnSubscribe(subscriber, publicKey.toBase64String()).doOnSubscribe();
         }
     }
 
-    private class OnSubscribe(val subscriber: Subscriber<in Unit>, val publicKey: PublicKey) {
+    private class OnSubscribe(val subscriber: Subscriber<in Unit>, val publicKeyString: String) {
+
+        val email = publicKeyString + "@safechat.com"
+        val password = publicKeyString + "safe"
+
         fun doOnSubscribe() {
             subscriber.onStart()
+            register()
+        }
+
+        private fun register() {
+            FirebaseAuth
+                    .getInstance()
+                    .createUserWithEmailAndPassword(email, password)
+                    .addOnSuccessListener(logIn)
+                    .addOnFailureListener(onError)
+        }
+
+        private val logIn: (AuthResult) -> Unit = {
+            FirebaseAuth
+                    .getInstance()
+                    .signInWithEmailAndPassword(email, password)
+                    .addOnSuccessListener(putUserToDataBase)
+                    .addOnFailureListener(onError)
+        }
+
+        private val putUserToDataBase: (AuthResult) -> Unit = {
             FirebaseDatabase
                     .getInstance()
                     .reference
                     .child("users")
-                    .child("UID-" + publicKey.toBase64String().take(40))
-                    .setValue(EmptyUser(publicKey.toBase64String()))
+                    .child(it.user.uid)
+                    .setValue(EmptyUser(publicKeyString))
                     .addOnSuccessListener(onSuccess)
                     .addOnFailureListener(onError)
         }
 
-        val onSuccess: (Void?) -> Unit = {
+        private val onSuccess: (Void?) -> Unit = {
             subscriber.onNext(Unit)
             subscriber.onCompleted()
         }
-        val onError: (Exception) -> Unit = {
+
+        private val onError: (Exception) -> Unit = {
+            Log.e("Log error", it.toString(), it)
             subscriber.onError(it)
-            subscriber.onCompleted()
         }
     }
 
