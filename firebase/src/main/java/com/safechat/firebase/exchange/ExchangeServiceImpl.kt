6f25740d10398dbb8ed8b18721a2b60da4ba13c8
Karol Kowalski <ordon1313@gmail.com>
2016-06-06 15:56:00 +0200

    empty ExchangeServiceImpl

---
+++ firebase/src/main/java/com/safechat/firebase/exchange/ExchangeServiceImpl.kt
@@ -0,0 +1,17 @@
+package com.safechat.firebase.exchange
+
+import com.safechat.conversation.symmetrickey.post.PostSymmetricKeyService
+import com.safechat.conversation.symmetrickey.retrieve.RetrieveSymmetricKeyService
+import rx.Observable
+
+class ExchangeServiceImpl : PostSymmetricKeyService, RetrieveSymmetricKeyService {
+
+    override fun postSymmetricKey(myPublicKey: String, otherPublicKey: String, encryptedSymmetricKey: String): Observable<Unit> {
+        throw UnsupportedOperationException()
+    }
+
+    override fun retrieveSymmetricKey(myPublicKey: String, otherPublicKey: String): Observable<String?> {
+        throw UnsupportedOperationException()
+    }
+
+}
