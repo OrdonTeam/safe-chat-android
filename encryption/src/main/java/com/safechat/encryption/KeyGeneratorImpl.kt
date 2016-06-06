336f9e4274a1a6fe2c5f4c2c5f237804467b5b11
Karol Kowalski <ordon1313@gmail.com>
2016-06-06 23:54:35 +0200

    refactor

--- encryption/src/main/java/com/safechat/encryption/KeyGeneratorImpl.kt
+++ encryption/src/main/java/com/safechat/encryption/KeyGeneratorImpl.kt
@@ -3,8 +3,6 @@ package com.safechat.encryption
 import com.safechat.register.KeyGenerator
 import com.safechat.register.KeyPairString
 import rx.Observable
-import java.security.KeyPair
-import java.security.KeyPairGenerator
 
 class KeyGeneratorImpl : KeyGenerator {
 
