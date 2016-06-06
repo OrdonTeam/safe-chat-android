3b9863c86207b0bfc9bc1c6e323f916ec3b17be6
Karol Kowalski <ordon1313@gmail.com>
2016-06-06 18:21:47 +0200

    untested postSymmetricKeyToUserUid implementation

--- firebase/src/main/java/com/safechat/firebase/exchange/PostKey.kt
+++ firebase/src/main/java/com/safechat/firebase/exchange/PostKey.kt
@@ -1,7 +1,28 @@
 package com.safechat.firebase.exchange
 
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 import rx.Observable
 
-fun postSymmetricKeyToUserUid(myPublicKey: String, otherUid: String, encryptedSymmetricKey: String): Observable<Unit> {
-    throw UnsupportedOperationException("not implemented") //To change body of created functions use File | Settings | File Templates.
+fun postSymmetricKeyToUserUid(myPublicKey: String, otherUid: String, encryptedSymmetricKey: String) = Observable.create<Unit> { subscriber ->
+    subscriber.onStart()
+
+    val uid = FirebaseAuth.getInstance().currentUser!!.uid
+    FirebaseDatabase
+            .getInstance()
+            .reference
+            .child("users")
+            .child(otherUid)
+            .child("pending_requests")
+            .child(uid)
+            .setValue(PendingRequest(myPublicKey, encryptedSymmetricKey, "TODO"))
+            .addOnSuccessListener {
+                subscriber.onNext(Unit)
+                subscriber.onCompleted()
+            }
+            .addOnFailureListener {
+                subscriber.onError(it)
+            }
 }
+
+private data class PendingRequest(val rsa: String, val encryptedSymmetricKey: String, val conversationId: String)
