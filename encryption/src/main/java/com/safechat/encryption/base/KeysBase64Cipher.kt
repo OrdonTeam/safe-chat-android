e1d587897afb65980a22cd7ac06cfe14d4b122d2
Karol Kowalski <ordon1313@gmail.com>
2016-06-08 14:27:13 +0200

    decodeSymmetricKey

--- encryption/src/main/java/com/safechat/encryption/base/KeysBase64Cipher.kt
+++ encryption/src/main/java/com/safechat/encryption/base/KeysBase64Cipher.kt
@@ -6,6 +6,8 @@ import java.security.PrivateKey
 import java.security.PublicKey
 import java.security.spec.PKCS8EncodedKeySpec
 import java.security.spec.X509EncodedKeySpec
+import javax.crypto.SecretKey
+import javax.crypto.spec.SecretKeySpec
 
 object KeysBase64Cipher {
 
@@ -15,6 +17,10 @@ object KeysBase64Cipher {
 
     fun decodePrivateKey(base64String: String): PrivateKey {
         return KeyFactory.getInstance("RSA").generatePrivate(PKCS8EncodedKeySpec(Base64Encoder.decode(base64String)))
+    }
+
+    fun decodeSymmetricKey(base64String: String): SecretKey {
+        return SecretKeySpec(Base64Encoder.decode(base64String), "AES");
     }
 
     fun toBase64String(key: Key): String {
--- encryption/src/test/java/com/safechat/encryption/base/KeysBase64CipherTest.kt
+++ encryption/src/test/java/com/safechat/encryption/base/KeysBase64CipherTest.kt
@@ -2,17 +2,21 @@ package com.safechat.encryption.base
 
 import com.safechat.encryption.base.KeysBase64Cipher.decodePrivateKey
 import com.safechat.encryption.base.KeysBase64Cipher.decodePublicKey
+import com.safechat.encryption.base.KeysBase64Cipher.decodeSymmetricKey
 import com.safechat.encryption.base.KeysBase64Cipher.toBase64String
 import com.safechat.encryption.generator.newKeyPair
+import com.safechat.encryption.generator.newSymmetricKey
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class KeysBase64CipherTest {
 
     val keyPair = newKeyPair()
+    val symmetricKey = newSymmetricKey()
 
     @Test
     fun shouldEncryptAndDecryptKeysToBase64() {
+        assertEquals(symmetricKey, decodeSymmetricKey(toBase64String(symmetricKey)))
         assertEquals(keyPair.public, decodePublicKey(toBase64String(keyPair.public)))
         assertEquals(keyPair.private, decodePrivateKey(toBase64String(keyPair.private)))
     }
