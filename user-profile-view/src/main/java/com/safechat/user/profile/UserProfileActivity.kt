65ebc84f36547317c8bfbb49f9e40f4728bd6940
Karol Kowalski <ordon1313@gmail.com>
2016-06-27 00:49:23 +0200

    Implement UserProfileActivity.showError

--- user-profile-view/src/main/java/com/safechat/user/profile/UserProfileActivity.kt
+++ user-profile-view/src/main/java/com/safechat/user/profile/UserProfileActivity.kt
@@ -3,6 +3,8 @@ package com.safechat.user.profile
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.support.design.widget.CoordinatorLayout
+import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.widget.TextView
@@ -10,12 +12,13 @@ import android.widget.TextView
 
 class UserProfileActivity : AppCompatActivity(), UserProfileView {
 
+    val controller by lazy { userProfileControllerProvider(this) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.user_profile)
         setSupportActionBar(findViewById(R.id.toolbar) as Toolbar)
         supportActionBar!!.setTitle(R.string.user_profile_title)
-        val controller = userProfileControllerProvider(this)
         controller.onCreate()
     }
 
@@ -24,7 +27,10 @@ class UserProfileActivity : AppCompatActivity(), UserProfileView {
     }
 
     override fun showError() {
-        throw UnsupportedOperationException()
+        val coordinator = findViewById(R.id.user_profile_coordinator) as CoordinatorLayout
+        Snackbar.make(coordinator, R.string.user_profile_error, Snackbar.LENGTH_INDEFINITE)
+                .setAction(R.string.try_again, { controller.onCreate() })
+                .show()
     }
 
     companion object {
