cb67eeefd68b553d5d3f443783c9cf25f880b56a
ghostbuster91 <kghost0@gmail.com>
2016-06-26 10:58:33 +0200

    create SuidCalculator

---
+++ suid-calculator/src/main/java/com/safechat/suid/generator/SuidCalculator.kt
@@ -0,0 +1,27 @@
+package com.safechat.suid.generator
+
+class SuidCalculator {
+
+    fun findShortestUniqueSubstring(original: String, others: List<String>) =
+            generateSubstrings(original)
+                    .first { substring ->
+                        isNotContainedByAnyElementOfList(substring, others)
+                    }
+
+    private fun isNotContainedByAnyElementOfList(substring: String, others: List<String>) =
+            others.any {
+                it.contains(substring)
+            }.not()
+
+    fun generateSubstrings(input: String) =
+            input.mapIndexed { index, letter ->
+                input.substring(0, input.length - index)
+            }.flatMap { text ->
+                text.mapIndexed { index, letter ->
+                    text.substring(index, text.length)
+                }
+            }.toSet()
+                    .filter { it.length > 2 }
+                    .sortedBy { it.length }
+
+}
---
+++ suid-calculator/src/test/java/com/safechat/suid/generator/SuidCalculatorTest.kt
@@ -0,0 +1,26 @@
+package com.safechat.suid.generator
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class SuidCalculatorTest {
+
+    val suidCalculator = SuidCalculator()
+
+    @Test
+    fun shouldGenerateAllSubstringsLongerThanTwo() {
+        val input = "kasper"
+
+        assertEquals(setOf("kasper", "kaspe", "kasp", "kas",
+                "asper", "aspe", "asp",
+                "sper", "spe",
+                "per"), suidCalculator.generateSubstrings(input).toSet())
+    }
+
+    @Test
+    fun shouldFindShortestUniqueSubstring() {
+        val input = listOf("kasper", "kas2per")
+
+        assertEquals("mas", suidCalculator.findShortestUniqueSubstring("masper", input))
+    }
+}
