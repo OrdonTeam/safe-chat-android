4bf5a6e80187ac08ae11026313bd0ecf9302d2fd
Karol Kowalski <ordon1313@gmail.com>
2016-07-23 10:06:09 +0200

    fix #29 - secure password with two random UUIDs

--- firebase/src/main/java/com/safechat/firebase/register/RegisterServiceImpl.kt
+++ firebase/src/main/java/com/safechat/firebase/register/RegisterServiceImpl.kt
@@ -5,12 +5,13 @@ import com.safechat.firebase.auth.loginUser
 import com.safechat.firebase.users.putUser
 import com.safechat.register.RegisterService
 import rx.Observable
+import java.util.*
 
 class RegisterServiceImpl : RegisterService {
 
     override fun registerNewKey(publicKey: String): Observable<Unit> {
         val email = publicKey + "@safechat.com"
-        val password = publicKey + "safe"
+        val password = UUID.randomUUID().toString() + UUID.randomUUID().toString()
         return createUser(email, password)
                 .flatMap { loginUser(email, password) }
                 .flatMap { putUser(it.user.uid, publicKey) }
